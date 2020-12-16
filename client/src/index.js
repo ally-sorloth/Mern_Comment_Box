e032c39e2c486a204e3a5561745c8b748ee5aebb
ally-sorloth <allysorloth@gmail.com>
2020-12-17 02:30:43 +0300

    create new file in client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CommentBox from './CommentBox';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CommentBox />
   </React.StrictMode>,
   document.getElementById('root')
 );
